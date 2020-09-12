a029c7c1777b2fd2b904b7a502d18d0ef51d77a8
yangtian <yangtian@hzguct.com>
2020-09-12 13:40:18 +0800

    commit

--- ptt/src/main/java/com/witted/bean/User.java
+++
@@ -1,6 +0,0 @@
-package com.witted.bean;
-
-public class User {
-
-
-}
--- ptt/src/main/java/com/witted/constant/StatusCode.java
+++
@@ -1,7 +0,0 @@
-package com.witted.constant;
-
-public interface StatusCode{
-
-    int SERVICE_DIED=-1001;
-    int TCP_CONNECT_ERR=1;
-}
