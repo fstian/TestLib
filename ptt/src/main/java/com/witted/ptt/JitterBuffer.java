d4dfc6a7687d77beb319df0ee25dc436fce9d4c6
yangtian <yangtian@hzguct.com>
2020-11-18 13:41:07 +0800

    test

--- ptt/src/main/java/com/witted/ptt/JitterBuffer.java
+++ ptt/src/main/java/com/witted/ptt/JitterBuffer.java
@@ -1,101 +1,29 @@
 package com.witted.ptt;
 
+import android.util.Log;
+
+
+
 public class JitterBuffer {
 
+
+    private static final String TAG ="JitterBuffer" ;
+
     static {
-        System.loadLibrary("JitterBuffer");
-        getInstance();
-    }
-
-    Object mObject=new Object();
-
-
-    static JitterBuffer jb;
-
-
-    public JitterBuffer() {
+        System.loadLibrary("jitterbuffer");
         initJb();
+        Log.i(TAG, "static initializer: ");
     }
 
-    public static JitterBuffer getInstance(){
-        if(jb==null){
-            jb=new JitterBuffer();
-        }
-        return jb;
-    }
+    public static native int initJb();
 
+    public static native int openJb(int codec,int ptime);
+    public static native int closeJb(int jbline);
 
+    public static native int addPackage(int jbline,byte[] src,int size);
 
-    private native int InitJb();
-    private native int OpenJb(int codec,int ptime);
-    private native int CloseJb(int jbline);
-    private native int ResetJb(int jbline);
-    private native int AddPackage(int jbline,byte[] src,int size);
-    private native int GetPackage(int jbline,byte[] dst,int size);
-    private native int GetStatus(int jbline,byte[] dst,int size);
-    private native int GetJbVer();
-    private native int DeInitJb();
-
-    public int initJb(){
-        synchronized (mObject)
-        {
-            return InitJb();
-        }
-    }
-
-    public int openJb(int codec,int ptime){
-        synchronized (mObject)
-        {
-            return OpenJb(codec,ptime);
-        }
-    }
-
-    public int resetJb(int jbline){
-        synchronized (mObject)
-        {
-            return ResetJb(jbline);
-        }
-    }
-
-    public  int closeJb(int jbline){
-        synchronized (mObject)
-        {
-            return CloseJb(jbline);
-        }
-    }
-
-    public int addPackage(int jbline,byte[] src,int size){
-        synchronized (mObject)
-        {
-            return AddPackage(jbline,src,size);
-        }
-    }
-
-    public  int getPackage(int jbline,byte[] dst,int size){
-        synchronized (mObject)
-        {
-            return GetPackage(jbline,dst,size);
-        }
-    }
-    public int getStatus(int jbline,byte[] dst,int size){
-        synchronized (mObject)
-        {
-            return GetStatus(jbline,dst,size);
-        }
-
-    }
-    public int getJbVer(){
-        synchronized (mObject)
-        {
-            return GetJbVer();
-        }
-    }
-    public int deInitJb(){
-        synchronized (mObject)
-        {
-            return DeInitJb();
-        }
-
-    }
-
+    public static native int getPackage(int jbline,byte[] dst,int size);
+    public static native int getStatus(int jbline,byte[] dst,int size);
+    public static native int getJbVer();
+    public static native int deInitJb();
 }
